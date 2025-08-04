01dc716d9c30df70cc45c001871845fab4d8365b
SaridiSowmya <sowmya.saridi@atyeti.com>
2025-08-04 14:56:26 +0530

    Delete CSVfileImporterandValidator directory

--- CSVfileImporterandValidator/CSVfileImporterandValidator/src/main/java/com/example/CSVfileImporterandValidator/exception/CSVValidationException.java
+++
@@ -1,7 +0,0 @@
-package com.example.CSVfileImporterandValidator.exception;
-
-public class CSVValidationException extends RuntimeException {
-    public CSVValidationException(String message) {
-        super(message);
-    }
-}
--- CSVfileImporterandValidator/CSVfileImporterandValidator/src/main/java/com/example/CSVfileImporterandValidator/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.CSVfileImporterandValidator.repository;
-
-import com.example.CSVfileImporterandValidator.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Integer> {
-}
