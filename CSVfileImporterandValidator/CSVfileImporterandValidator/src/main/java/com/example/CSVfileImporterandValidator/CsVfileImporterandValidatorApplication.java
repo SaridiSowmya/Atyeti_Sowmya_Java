e6efc94c2f3421a8c84c239fab6a5f73d74ae047
SaridiSowmya <sowmya.saridi@atyeti.com>
2025-08-04 15:15:16 +0530

    Add files via upload

---
+++ CSVfileImporterandValidator/CSVfileImporterandValidator/src/main/java/com/example/CSVfileImporterandValidator/CsVfileImporterandValidatorApplication.java
@@ -0,0 +1,13 @@
+package com.example.CSVfileImporterandValidator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CsVfileImporterandValidatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CsVfileImporterandValidatorApplication.class, args);
+	}
+
+}
